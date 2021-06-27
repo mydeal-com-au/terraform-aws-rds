e3da551a2606c9a7119fd11522aa517b9f842018
maicon.rocha@dnx.solutions <maicon.rocha@dnx.solutions>
2021-06-27 06:03:02 +0000

    adding custom cluster and parameter group resources

--- _variables.tf
+++ _variables.tf
@@ -58,10 +58,10 @@ variable "port" {
   description = "Port number for this DB (usually 3306 for MySQL and 5432 for Postgres)"
 }
 
-variable "parameter_group_name" {
-  type        = string
-  description = "Parameter group name for this DB"
-}
+#variable "parameter_group_name" {
+#  type        = string
+#  description = "Parameter group name for this DB"
+#}
 
 variable "apply_immediately" {
   type        = bool
@@ -147,4 +147,97 @@ variable "count_aurora_instances" {
   description = "Number of Aurora Instances"
   type        = number
   default     = "1"
+}
+
+# DB and Cluster parameter group
+variable "create_cluster_parameter_group" {
+  description = "Whether to create a cluster parameter group"
+  type        = bool
+  default     = false
+}
+
+variable "cluster_parameters" {
+  description = "A list of Cluster parameters (map) to apply"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "create_db_parameter_group" {
+  description = "Whether to create a database parameter group"
+  type        = bool
+  default     = false
+}
+
+variable "db_parameters" {
+  description = "A list of DB parameters (map) to apply"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "parameter_group_name" {
+  description = "Name of the DB parameter group to associate or create"
+  type        = string
+  default     = null
+}
+
+#variable "use_name_prefix" {
+#  description = "Determines whether to use `parameter_group_name` as is or create a unique name beginning with `parameter_group_name` as the specified prefix"
+#  type        = bool
+#  default     = true
+#}
+
+variable "parameter_group_description" {
+  description = "The description of the DB parameter group"
+  type        = string
+  default     = ""
+}
+
+variable "family" {
+  description = "The family of the DB parameter group"
+  type        = string
+  default     = ""
+}
+
+# DB option group
+variable "create_db_option_group" {
+  description = "(Optional) Create a database option group"
+  type        = bool
+  default     = true
+}
+
+variable "option_group_name" {
+  description = "Name of the option group"
+  type        = string
+  default     = null
+}
+
+variable "option_group_use_name_prefix" {
+  description = "Determines whether to use `option_group_name` as is or create a unique name beginning with the `option_group_name` as the prefix"
+  type        = bool
+  default     = true
+}
+
+variable "option_group_description" {
+  description = "The description of the option group"
+  type        = string
+  default     = ""
+}
+
+variable "major_engine_version" {
+  description = "Specifies the major version of the engine that this option group should be associated with"
+  type        = string
+  default     = ""
+}
+
+variable "options" {
+  description = "A list of Options to apply."
+  type        = any
+  default     = []
+}
+
+
+variable "deletion_protection" {
+  description = "The database can't be deleted when this value is set to true."
+  type        = bool
+  default     = false
 }
--- aurora.tf
+++ aurora.tf
@@ -12,13 +12,62 @@ resource "aws_rds_cluster" "aurora_cluster" {
   snapshot_identifier                 = var.snapshot_identifier != "" ? var.snapshot_identifier : null
   db_subnet_group_name                = try(aws_db_subnet_group.rds_subnet_group[0].id, var.db_subnet_group_id)
   iam_database_authentication_enabled = var.iam_database_authentication_enabled
+  vpc_security_group_ids              = [aws_security_group.rds_db.id]
+  db_cluster_parameter_group_name     = var.create_cluster_parameter_group == true ? aws_rds_cluster_parameter_group.custom_cluster_pg[count.index].name : ""
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
-  count              = var.db_type == "aurora" ? var.count_aurora_instances : 0
-  identifier         = "${var.identifier}-${count.index}"
-  cluster_identifier = aws_rds_cluster.aurora_cluster[0].id
-  instance_class     = var.instance_class
-  engine             = aws_rds_cluster.aurora_cluster[0].engine
-  engine_version     = aws_rds_cluster.aurora_cluster[0].engine_version
+  count                   = var.db_type == "aurora" ? var.count_aurora_instances : 0
+  identifier              = "${var.identifier}-${count.index}"
+  cluster_identifier      = aws_rds_cluster.aurora_cluster[0].id
+  instance_class          = var.instance_class
+  engine                  = aws_rds_cluster.aurora_cluster[0].engine
+  engine_version          = aws_rds_cluster.aurora_cluster[0].engine_version
+  db_parameter_group_name = var.create_db_parameter_group == true ? aws_db_parameter_group.custom_db_pg[count.index].name : ""
+
+}
+
+resource "aws_rds_cluster_parameter_group" "custom_cluster_pg" {
+  count = var.create_cluster_parameter_group ? 1 : 0
+
+  name = var.parameter_group_name
+  #name_prefix = local.name_prefix
+  description = var.parameter_group_description
+  family      = var.family
+
+  dynamic "parameter" {
+    for_each = var.cluster_parameters
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+      apply_method = lookup(parameter.value, "apply_method", null)
+    }
+  }
+
+}
+
+resource "aws_db_parameter_group" "custom_db_pg" {
+  count = var.create_db_parameter_group ? 1 : 0
+
+  name = var.parameter_group_name
+  #name_prefix = local.name_prefix
+  description = var.parameter_group_description
+  family      = var.family
+
+  dynamic "parameter" {
+    for_each = var.db_parameters
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+      apply_method = lookup(parameter.value, "apply_method", null)
+    }
+  }
+
+  tags = {
+    "Name" = var.parameter_group_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
