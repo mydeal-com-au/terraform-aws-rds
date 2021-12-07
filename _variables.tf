9838ac574b9e3e548da3ec995be863a004ea79f8
Fabio Ramos <fabio.d.o.ramos@gmail.com>
2021-12-07 18:14:44 +1100

    update var description

--- _variables.tf
+++ _variables.tf
@@ -287,7 +287,7 @@ variable "publicly_accessible" {
 }
 
 variable "license_model" {
-  description = "License Model"
+  description = "License model information for this DB instance (Optional, but required for some DB engines, i.e. Oracle SE1 and SQL Server)"
   type        = string
   default     = ""
 }
